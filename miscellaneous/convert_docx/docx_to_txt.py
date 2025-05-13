622d76d9e2bd31e097ba36bc8d454b5a403c5f13
Zhao Li <43864970+Zhaoli2042@users.noreply.github.com>
2025-05-13 03:08:32 -0400

    Add files via upload

--- miscellaneous/convert_docx/docx_to_txt.py
+++ miscellaneous/convert_docx/docx_to_txt.py
@@ -2,7 +2,10 @@ import os
 import glob
 import docx2txt      # lightweight, pure-Python DOCX extractor
 
-def DOCX_TO_TXT(source_file, destination_file):
+def DOCX_TO_TXT(source_file):   
+    without_ext = source_file.with_suffix("")          # PosixPath('XXX/XXX')
+    destination_file = without_ext.with_suffix(".txt")
+    print(f"DESTINATION: {destination_file}", flush = True)
     txt = docx2txt.process(source_file)          # extract plain text
     with open(destination_file, "w", encoding="utf-8") as f:
         f.write(txt)
